06883cdac6d66938d80d6c3f4b6f8ac97e33d621
Shell M. Shrader <shell@shellware.com>
2012-01-05 00:47:22 -0500

    initial core code commit

---
+++ src/com/shellware/ARPro/Firmware/firmware.java
@@ -0,0 +1,30 @@
+package com.shellware.ARPro.Firmware;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class firmware extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        final Button marketButton = (Button) findViewById(R.id.button1);
+        
+        marketButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View arg0) {
+	     	   	try {
+					Intent getFw = new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=com.shellware.AR.Pro"));
+					startActivity(getFw);
+				} catch (Exception e) {
+					// do nothing
+				}
+	        }
+        });
+    }
+}
