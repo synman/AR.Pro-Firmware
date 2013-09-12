625492aea43ee39a62a3cad0387ce6229ab21ce6
Shell M. Shrader <shell@shellware.com>
2013-09-12 00:02:33 -0400

    amazon additions

--- src/com/shellware/ARPro/Firmware/firmware.java
+++ src/com/shellware/ARPro/Firmware/firmware.java
@@ -8,19 +8,35 @@ import android.view.View;
 import android.widget.Button;
 
 public class firmware extends Activity {
-    /** Called when the activity is first created. */
+	
+	public final static boolean KINDLE_VERSION = true;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
         final Button marketButton = (Button) findViewById(R.id.button1);
+
+        if (KINDLE_VERSION) {
+        	marketButton.setText(R.string.arpro_amazon);
+        } else {
+        	marketButton.setText(R.string.arpro_market);        	
+        }
+        
         
         marketButton.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View arg0) {
 	     	   	try {
-//					Intent getFw = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.amazon.com/gp/mas/dl/android?p=com.shellware.AR.Pro.Fire"));
-					Intent getFw = new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=com.shellware.AR.Pro"));
+	     	   		Intent getFw;
+	     	   		
+	     	   		if (KINDLE_VERSION) {
+						 getFw = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.amazon.com/gp/mas/dl/android?p=com.shellware.AR.Pro.Fire"));
+	     	   			
+	     	   		} else {
+						 getFw = new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=com.shellware.AR.Pro"));	     	   			
+	     	   		}
+	     	   		
 					startActivity(getFw);
 				} catch (Exception e) {
 					// do nothing
